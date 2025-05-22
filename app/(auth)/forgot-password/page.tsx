186a0e7f94701604c6a603c480f335109ec566be
Saad <saadksioui@gmail.com>
2025-05-22 22:27:07 +0100

    feat(auth): implement authentication flow with OTP verification and error handling feat(auth): enhance error page with user-friendly messages feat(auth): add sign-up, login, forgot password, and update password forms feat(ui): create reusable card component for consistent UI presentation

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,10 +1,11 @@
+import ForgotPasswordForm from "../_components/forgot-password-form";
 
-const Page = () => {
+export default function Page() {
   return (
-    <div>
-      Page
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <ForgotPasswordForm />
+      </div>
     </div>
   )
-};
-
-export default Page
+}
--- app/(auth)/update-password/page.tsx
+++ app/(auth)/update-password/page.tsx
@@ -1,10 +1,12 @@
+import UpdatePasswordForm from "../_components/update-password-form";
 
-const Page = () => {
+
+export default function Page() {
   return (
-    <div>
-      Page
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <UpdatePasswordForm />
+      </div>
     </div>
   )
-};
-
-export default Page
+}
